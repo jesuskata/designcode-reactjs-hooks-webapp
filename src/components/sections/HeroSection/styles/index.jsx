076a0751e17d151769fed56e3c44b4c4f25b5ed2
jesuskata <rzarate.alex@gmail.com>
2021-01-22 20:51:35 -0600

    style(styles): 💄 create the stayled components for herosection

---
+++ src/components/sections/HeroSection/styles/index.jsx
@@ -0,0 +1,29 @@
+// Dependencies
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316DB 0%, #9076E7 100%);
+`;
+
+export const ContentWrapper = styled.div`
+  max-width: 1234px;
+  margin: 0 auto;
+  padding: 200px 30px;
+`;
+
+export const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px
+`;
+
+export const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: white;
+`;
+
+export const Description = styled.p`
+  font-size: 17px;
+  line-height: 130%;
+`;
