7b733e1e8a66c6675f8399075133938ae3b3e253
jesuskata <rzarate.alex@gmail.com>
2021-02-05 11:24:33 -0600

    style(styles): 💄 add animations for the textwrapper

--- src/components/sections/HeroSection/styles/index.jsx
+++ src/components/sections/HeroSection/styles/index.jsx
@@ -1,9 +1,14 @@
 // Dependencies
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 // Styled Components
 import { H1, MediumText } from '../../../styles/TextStyles';
 import { themes } from '../../../styles/ColorStyles';
+
+const animation = keyframes`
+  from { opacity: 0; transform: translateY(-10px); filter: blur(10px); }
+  to { opacity: 1; transform: translateY(0px); filter: blur(0px); }
+`;
 
 export const Wrapper = styled.div`
   overflow: hidden;
@@ -21,6 +26,23 @@ export const TextWrapper = styled.div`
   max-width: 360px;
   display: grid;
   gap: 30px;
+
+  > * {
+    opacity: 0;
+    animation: ${animation} 1s forwards;
+
+    :nth-child(1) {
+      animation-delay: 0s;
+    }
+
+    :nth-child(2) {
+      animation-delay: 0.2s;
+    }
+
+    :nth-child(3) {
+      animation-delay: 0.5s;
+    }
+  }
 `;
 
 export const Title = styled(H1)`
